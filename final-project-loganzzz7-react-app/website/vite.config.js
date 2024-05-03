fd45a2561c9e899cb3dd0ca9c15e1c58272d163e
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2024-05-02 20:47:55 -0500

    run deploy

--- final-project-loganzzz7-react-app/website/vite.config.js
+++ final-project-loganzzz7-react-app/website/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/final-project-loganzzz7_finalprojectapp/',
+  plugins: [react()]
+});
