2494609e78929d434ffa2836b4b97137c6102f1c
loganzzz7 <112505545+loganzzz7@users.noreply.github.com>
2024-05-02 20:30:54 -0500

    DONE!!!

--- final-project-loganzzz7-react-app/backend/server.js
+++ final-project-loganzzz7-react-app/backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+// mailchimp
 import mailchimp from '@mailchimp/mailchimp_marketing';
+// dialogflow
+import { SessionsClient } from '@google-cloud/dialogflow';
+import { v4 as uuidv4 } from 'uuid';
+
 
 dotenv.config();
 
+// MAILCHIMP API config
 // api = https://mailchimp.com/developer/marketing/guides/quick-start/#create-an-account
 mailchimp.setConfig({
     apiKey: process.env.MAILCHIMP_API_KEY,
@@ -14,16 +20,13 @@ mailchimp.setConfig({
 const app = express();
 
 const port = 8080; // idk y but airplay takes up port 5000 and 7000??
-// check port
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-});
 
 app.use(cors());
 app.use(express.json());
 
+// add sub path
 app.post('/addSubscriber', async (req, res) => {
-    // console.log("Request body:", req.body);
+    // console.log("entire req test:", req.body);
     const {email, firstName, lastName} = req.body;
 
     try {
@@ -45,4 +48,47 @@ app.post('/addSubscriber', async (req, res) => {
         console.error('error:', error);
         res.status(500).json({ message: "add sub fail", error: error.toString() });
     }
+});
+
+// DIALOGFLOW API
+// https://googleapis.dev/nodejs/dialogflow/latest/v2.SessionsClient.html 
+// file from googlecloud => credentials
+const dialogFlow = new SessionsClient({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
+});
+
+// chat path
+app.post('/messageBot', async (req, res) => {
+    const { message } = req.body;
+    const sessionID = uuidv4(); // random session id for user, no need to track convo
+    const sessionPath = dialogFlow.projectAgentSessionPath(process.env.GOOGLE_PROJECT_ID, sessionID);
+
+    const request = {
+        session: sessionPath,
+        queryInput: {
+            text: {
+                text: message,  // user input
+                languageCode: 'en-US', // msg lan
+            },
+        },
+    };
+
+    try {
+        const dialogFlowResponse = await dialogFlow.detectIntent(request);
+        const result = dialogFlowResponse[0].queryResult;
+        // result.fulfillmentText gets the set up responses on dialogflow
+        const setResponseTxt = result.fulfillmentText
+
+        // log to test
+        console.log('check response:', setResponseTxt)
+        res.status(200).json({ reply: setResponseTxt });
+    } catch (error) {
+        console.error('Dialogflow error:', error);
+        res.status(500).json({ message: "message failed", error: error.toString() });
+    }
+});
+
+// check port
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
 });
--- final-project-loganzzz7-react-app/website/src/Chatbot.jsx
+++ final-project-loganzzz7-react-app/website/src/Chatbot.jsx
@@ -6,15 +6,72 @@ import { useState } from "react";
 // chatbot popup tut: https://safwan-du16.medium.com/chat-popups-on-react-ever-reuseable-components-f9c99a7ac94
 // wanted to use dialogflow + kommunicate but I think that it might have been too simple?
 function Chatbot() {
-    const onChatBtnClick = () => {
+    const [chatBoxOpen, setChatBoxOpen] = useState(false);
+    const [messagesSent, setMessagesSent] = useState([]); // track msg history
+    const [newMessage, setNewMessage] = useState(''); // new user msg
 
+    const onChatBtnClick = () => {
+        setChatBoxOpen(!chatBoxOpen);
+    }
+
+    const onUserInputChange = (event) => {
+        setNewMessage(event.target.value);
+    }
+
+    const enterKeyPress = (event) => {
+        if (event.key == 'Enter') {
+            event.preventDefault();
+            onSendMessage(); // allow to send msg when press enter key
+        }
+    }
+
+    const onSendMessage = async () => {
+        const messageInputed = newMessage;
+        setMessagesSent([...messagesSent, { text: newMessage, from: 'user' }]); // add usermsg to msg hist
+        // clear after send
+        setNewMessage('');
+
+        try {
+            const response = await fetch('http://localhost:8080/messageBot', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    message: messageInputed
+                }),
+            });
+
+            const data = await response.json();
+            console.log(data);
+            setMessagesSent(messages => [...messages, { text: data.reply, from: 'bot' }]); // gets set response from dialogflow
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 
     return (
         <>
-        <div className="chatbotFAB">
-            <button onClick={onChatBtnClick}> <FontAwesomeIcon icon={faHeadset} /> </button>
-        </div>
+            <div className="chatbotFAB">
+                <button onClick={onChatBtnClick}> <FontAwesomeIcon icon={faHeadset} /> </button>
+            </div>
+            {chatBoxOpen && (
+                <div className="chatBox">
+                    <div className="chatMessages">
+                        {messagesSent.map((msg, index) => (
+                            <div key={index} className={`message ${msg.from}`}>
+                                {/* ran out of time to swap a pic in for user and another pic in for FAQbot */}
+                                {msg.from === 'user' ? 'User:' : 'FAQ bot:' }&nbsp;{msg.text}
+                            </div>
+                        ))}
+                    </div>
+                    <div className="sendMessage">
+                        <input type="text" value={newMessage} onChange={onUserInputChange}
+                            onKeyDown={enterKeyPress} />
+                        <button onClick={onSendMessage}>Send</button>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
